0cc48a9f19574f22d3e04a5dfe530afb69da9060
Pooja Venkatesh Naik <1rn21cs106.pooja@gmail.com>
2024-03-16 16:28:34 +0530

    Initial commit of Women Self-Help Group Projec
    This commit represents the initial upload of the Women Self-Help Group project files to GitHub. The project is a web application developed using Flask, MySQL, HTML, CSS, and JavaScript.
    
    The current version includes basic functionalities such as user signup, login, and group creation. However, the project is still a work in progress and does not yet feature all planned functionalities.
    
    Future updates will include:
    
    Implementation of group management features
    Integration of user profile pages
    Enhancements to the user interface and user experience
    Implementation of server-side validation for user input
    Integration of additional security measures
    financial management
    event management
    
    Please note that the project is not yet completed, and further development work is ongoing. Feedback and contributions from collaborators are welcome.

---
+++ Women_SFG/app.py
@@ -0,0 +1,188 @@
+import json 
+from urllib.parse import urlparse
+from flask import Flask,render_template,request,session,redirect, url_for, flash
+import mysql.connector 
+import pandas as pd
+import numpy as np 
+import secrets 
+
+app=Flask(__name__)
+app.secret_key = 'RNSIT#123hpooja'
+mydb=mysql.connector.connect(
+    host="localhost",
+    user="root",
+    passwd="RNSIT#123h",
+    database="women1",
+)
+global mycursor
+mycursor=mydb.cursor()
+
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+
+@app.route('/about.html')
+def about():
+    return render_template("about.html")
+
+@app.route('/group.html')
+def group():
+    return render_template("group.html")
+    
+
+@app.route('/manageGroup.html')
+def mg():
+    return render_template("manageGroup.html")
+
+
+@app.route('/grp_info.html')
+def Grp_info():
+    return render_template("grp_info.html")
+
+
+# ... (Your existing routes)
+@app.route('/Grp_signin', methods=['POST'])
+def grp_signin():
+    
+        # Get form data
+        username = request.form['In_username']
+        password = request.form['In_password']
+        
+        # Connect to MySQL database
+        
+        
+        # Query to check if username and password exist in the database
+        query = "SELECT * FROM ORG WHERE Username = %s AND Pass_word = %s"
+        mycursor.execute(query, (username, password))
+        user = mycursor.fetchone()
+        
+        if user:
+      
+          
+            return redirect('/grp_info.html')  # Change 'dashboard' to your desired route
+        else:
+            # If user does not exist, show error message
+           
+            flash('Invalid username or password. Please sign up first.', 'error')
+            return redirect('/manageGroup.html')  # Change 'signup' to your signup route
+        
+        cursor.close()
+        conn.close()
+
+
+
+@app.route('/Grp_signup', methods=['POST'])
+def grp_signup():
+    global gr_signup 
+    gr_signup=[]
+    user_name = request.form.get("username")
+    email_Add = request.form.get("emailAdd")
+    pass_word = request.form.get("password")
+    confirm_pass=request.form.get("confirmPass")
+    print(confirm_pass)
+    username_exists = check_username_exists(user_name)
+    password_exists=check_password_exists(pass_word)
+    mail_exists=check_mail_exists(email_Add)
+
+    if username_exists:
+        flash('Username already exists. Please choose a different username.', 'error')
+        return redirect('/group.html')
+    elif password_exists:
+        flash('Please choose any other password.', 'error')
+        return redirect('/group.html')
+    elif mail_exists:
+        flash('Already account exists with this mail , please choose other mail to continue.', 'error')
+        return redirect('/group.html')
+    elif pass_word!=confirm_pass:
+        flash('Password and Confirm Password do not match. Please make sure both passwords are identical.', 'error')
+        return redirect('/group.html')
+
+    else:
+        gr_signup.append(user_name)
+        gr_signup.append(email_Add)
+        gr_signup.append(pass_word)
+
+
+
+    signup_form = True
+    return render_template('group.html', signup_confirm=signup_form,username_exists=False)
+
+
+
+
+@app.route('/Grp_info', methods=['POST'])
+def grp_info():
+    
+    # Get data from the second form
+    group_Name = request.form.get("groupName")
+    stDate = request.form.get("st_date")
+    Intrest = request.form.get("interest")
+    Tot_no_memb = request.form.get("TotNo_memb")
+    group_location = request.form.get("Group_loc")
+    meeting_ptn = request.form.get("meetingPtn")
+
+    gr_signup.append(group_Name)
+    gr_signup.append(stDate)
+    gr_signup.append(Intrest)
+    gr_signup.append(Tot_no_memb)
+    gr_signup.append(group_location)
+    gr_signup.append(meeting_ptn)
+
+    values=(gr_signup[0],gr_signup[1],gr_signup[2],gr_signup[3],gr_signup[4],gr_signup[5],gr_signup[6],gr_signup[7],gr_signup[8])
+
+    query = "INSERT INTO ORG(Username, Mail_adress, Pass_word, Group_Name, Start_Date, intrest_perc, No_members, Grp_Location, Meet_ptn) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)"
+    mycursor.execute(query, values)
+    mydb.commit()
+
+    return render_template("group.html")
+
+
+
+def check_username_exists(username):
+    query1="SELECT * FROM ORG WHERE Username =%s"
+    mycursor.execute(query1,(username,))
+    result=mycursor.fetchone()
+    if not result:
+        return False 
+    else:
+        return True
+    
+def check_password_exists(password):
+    query2="SELECT * FROM ORG WHERE Pass_word=%s"
+    mycursor.execute(query2,(password,))
+    result=mycursor.fetchone()
+    if not result:
+        return False
+    else:
+        return True 
+    
+def check_mail_exists(mail_id):
+    query3="SELECT * FROM ORG WHERE Mail_adress=%s"
+    mycursor.execute(query3,(mail_id,))
+    result=mycursor.fetchone()
+    if not result:
+        return False
+    else:
+        return True
+
+        
+
+
+
+
+
+
+"""
+def organize():
+    table_data=[]
+    query = "SELECT * FROM organization;"
+    df = pd.read_sql(query, mydb)
+    table_data = df.to_dict(orient='records')
+    print(table_data)
+    return table_data
+"""
+
+if __name__ == '__main__':
+    app.run(debug=True)
